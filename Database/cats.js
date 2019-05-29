125603bce0b0ca9c2cac28ac36154dc4ed02cd0d
nrmarzo <nrmarzo@gmail.com>
2019-05-29 02:28:26 -0500

    Add mongoose to YelpCamp; Rename routes to be RESTful

---
+++ Database/cats.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cat_app', { useNewUrlParser: true });
+
+var catSchema = new mongoose.Schema({
+	name: String,
+	age: Number,
+	temperament: String
+});
+
+var Cat = mongoose.model('Cat', catSchema);
+
+// adding a new cat to the DB
+
+Cat.create(
+	{
+		name: 'Snow White',
+		age: 15,
+		temperament: 'Nice'
+	},
+	function(err, cat) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(cat);
+		}
+	}
+);
+
+// var george = new Cat({
+// 	name: 'Mrs. Norris',
+// 	age: 7,
+// 	temperament: 'Evil'
+// });
+
+// george.save(function(err, cat) {
+// 	if (err) {
+// 		console.log('Something went wrong');
+// 	} else {
+// 		console.log('We just saved a cat to the database');
+// 		console.log(cat);
+// 	}
+// });
+
+// retrieve all cats from the DB and console.log each one
+Cat.find({}, function(err, cats) {
+	if (err) {
+		console.log('ERROR');
+		console.log(err);
+	} else {
+		console.log('All the cats...');
+		console.log(cats);
+	}
+});
--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,51 +1,102 @@
-'use strict';
+"use strict";
 const PORT = 3000;
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
+let express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
-let campgrounds = [
-	{ name: 'Jenny Lake Camp', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
-	{ name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' },
-	{ name: "Mountain Goat's Rest", image: 'https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg' },
-	{ name: 'Jenny Lake Camp', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
-	{ name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' },
-	{ name: 'Jenny Lake Camp', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
-	{ name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' },
-	{ name: 'Jenny Lake Camp', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
-	{ name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' },
-	{ name: 'Jenny Lake Camp', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
-	{ name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' },
-	{ name: 'Jenny Lake Camp', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg' },
-	{ name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg' }
-];
-
-app.set('view engine', 'ejs');
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-	res.render('landing');
+// SCHEMA SETUP
+let campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
 });
 
-app.get('/campgrounds', function(req, res) {
-	res.render('campgrounds', { campgrounds: campgrounds });
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg",
+//     description: "This is a huge granite hill, no bathrooms. No water."
+//   },
+//   function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Newly created campground");
+//       console.log(campground);
+//     }
+//   }
+// );
+
+app.get("/", function(req, res) {
+  res.render("landing");
 });
 
-app.get('/campgrounds/new', function(req, res) {
-	res.render('new');
+// INDEX - Display all campgrounds
+app.get("/campgrounds", function(req, res) {
+  // Get all campgrounds from DB
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allCampgrounds });
+    }
+  });
+
+  // res.render("campgrounds", { campgrounds: campgrounds });
 });
 
-app.post('/campgrounds', function(req, res) {
-	// get data from form and add to campgrounds array
-	let name = req.body.name;
-	let image = req.body.image;
-	let newCampground = { name: name, image: image };
-	campgrounds.push(newCampground);
+// NEW - Show the form to add new campground
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new");
+});
 
-	// redirect back to campgrounds page
-	res.redirect('/campgrounds');
+// CREATE - Add new campground to DB
+app.post("/campgrounds", function(req, res) {
+  // get data from form and add to campgrounds array
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name: name, image: image };
+
+  // Create new campground and save to DB
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds page
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+  // Find the campground with provided ID
+  // Campground.findById(req.params.id, function(err, foun))
+  // render show template with that ID
+  res.render("show");
 });
 
 app.listen(PORT, function() {
-	console.log('YelpCamp Server has started on port ' + PORT);
+  console.log("YelpCamp Server has started on port " + PORT);
 });
+
+/*===========================================================================
+=============================================================================
+RESTFUL ROUTES
+
+name       url                 verb          desc.
+------------------------------------------------------------------------------
+INDEX     /campgrounds         GET           Display all campgrounds
+NEW				/campgrounds/new     GET           Show form to create new campgroud
+CREATE    /campgrounds         POST          Add new campground to DB
+SHOW			/campgrounds/:id     GET           Shows info about one campground
+
+=============================================================================
+===========================================================================*/
