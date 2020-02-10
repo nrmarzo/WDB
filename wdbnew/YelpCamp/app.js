19855272aa5705132de6fe0ad0956a6ef51f6248
nrmarzo <nrmarzo@gmail.com>
2020-02-10 12:52:51 -0600

    finished YelpCamp Basics

--- wdbnew/YelpCamp/app.js
+++ wdbnew/YelpCamp/app.js
@@ -1,25 +1,58 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 var campgrounds = [
   {
     name: "Salmon Creek",
     image:
-      "https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7d2f72d79f4bc45f_340.jpg"
+      "https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
   },
   {
     name: "Granite Hill",
     image:
-      "https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c7d2f72d79f4bc45f_340.jpg"
+      "https://images.unsplash.com/photo-1556335739-c95bf1b2f83f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
   },
   {
     name: "Mountain Goat's Rest",
     image:
-      "https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c7d2f72d79f4bc45f_340.jpg"
+      "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  },
+
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1556335739-c95bf1b2f83f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.unsplash.com/photo-1556335739-c95bf1b2f83f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image:
+      "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
   }
 ];
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.render("landing");
@@ -27,6 +60,21 @@ app.get("/", function(req, res) {
 
 app.get("/campgrounds", function(req, res) {
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+  // get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  // redirect back to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new");
 });
 
 app.listen(3000, function() {
