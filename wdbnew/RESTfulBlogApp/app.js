3520fbf7ef6c574a6eb8ca6a303036bfdba7feb3
nrmarzo <nrmarzo@gmail.com>
2020-02-24 22:18:51 -0600

    continued Restful blog app

--- wdbnew/RESTfulBlogApp/app.js
+++ wdbnew/RESTfulBlogApp/app.js
@@ -29,12 +29,42 @@ app.get("/", function(req, res) {
   res.redirect("/blogs");
 });
 
+// INDEX route
 app.get("/blogs", function(req, res) {
   Blog.find({}, function(err, blogs) {
     if (err) {
       console.log(err);
     } else {
       res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+// NEW route
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
+});
+
+// CREATE route
+app.post("/blogs", function(req, res) {
+  // create blog
+  // redirect to the index
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+// SHOW route
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
     }
   });
 });
