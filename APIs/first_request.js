3e3b13dbad0976d2fff445597c5603df13a58158
nrmarzo <nrmarzo@gmail.com>
2020-03-02 11:59:39 -0600

    Delete old wdb directories

--- APIs/first_request.js
+++
@@ -1,9 +0,0 @@
-('use strict');
-let request = require('request');
-
-request('https://jsonplaceholder.typicode.com/users/4', function(error, response, body) {
-	if (!error && response.statusCode == 200) {
-		const parsedData = JSON.parse(body);
-		console.log(parsedData.name + ' lives in ' + parsedData.address.city);
-	}
-});
--- IntroToNPM/DemoApp/app.js
+++
@@ -1,8 +0,0 @@
-var cat = require("cat-me");
-var joke = require("knock-knock-jokes");
-
-console.log(cat("grumpy"));
-
-console.log(cat());
-
-console.log(joke());
