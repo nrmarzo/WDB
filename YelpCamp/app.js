1f60b0cd8b33fe4597582fec67412fbe4743e691
nrmarzo <nrmarzo@gmail.com>
2020-03-04 11:37:23 -0600

    Added seeds file with comments

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,7 +2,10 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
-  Campground = require("./models/campground");
+  Campground = require("./models/campground"),
+  seedDB = require("./seeds");
+
+seedDB();
 
 mongoose.connect("mongodb://localhost/yelp_camp", {
   useUnifiedTopology: true,
@@ -12,41 +15,6 @@ mongoose.set("useUnifiedTopology", false);
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// var campgrounds = [
-//   {
-//     name: "Salmon Creek",
-//     image:
-//       "https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-//   },
-//   {
-//     name: "Granite Hill",
-//     image:
-//       "https://images.unsplash.com/photo-1556335739-c95bf1b2f83f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-//   },
-//   {
-//     name: "Mountain Goat's Rest",
-//     image:
-//       "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-//   }
-// ];
-
-// Campground.create(
-//   {
-//     name: "Granite Hill",
-//     image:
-//       "https://images.unsplash.com/photo-1556335739-c95bf1b2f83f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-//     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
-//   },
-//   function(err, campground) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("NEWLY CREATED CAMPGROUND");
-//       console.log(campground);
-//     }
-//   }
-// );
 
 app.get("/", function(req, res) {
   res.render("landing");
@@ -91,14 +59,17 @@ app.get("/campgrounds/new", function(req, res) {
 // SHOW - show info about one campground
 app.get("/campgrounds/:id", function(req, res) {
   // find the campground with provided ID
-  Campground.findById(req.params.id, function(err, foundCampground) {
-    if (err) {
-      console.log(err);
-    } else {
-      // render show template with that campground
-      res.render("show", { campground: foundCampground });
-    }
-  });
+  Campground.findById(req.params.id)
+    .populate("comments")
+    .exec(function(err, foundCampground) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(foundCampground);
+        // render show template with that campground
+        res.render("show", { campground: foundCampground });
+      }
+    });
 });
 
 app.listen(3000, function() {
