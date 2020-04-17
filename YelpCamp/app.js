cbe414ad463a94509963e6f302fc2606ac6606cb
nrmarzo <nrmarzo@gmail.com>
2020-04-16 23:46:04 -0500

    Added register and login functionality

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,22 +2,46 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
   Comment = require("./models/comment"),
   Campground = require("./models/campground"),
+  User = require("./models/user"),
   seedDB = require("./seeds");
-
-seedDB();
 
 mongoose.connect("mongodb://localhost/yelp_camp", {
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 mongoose.set("useUnifiedTopology", false);
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
+seedDB();
+
+// ===========================
+// PASSPORT CONFIGURATION
+// ===========================
+app.use(
+  require("express-session")({
+    secret: "Alexis is the cutest among the cutest",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ===========================
+// ROUTES
+// ===========================
+
+app.get("/", function (req, res) {
   res.render("landing");
 });
 
@@ -26,9 +50,9 @@ app.get("/", function(req, res) {
 // ===========================
 
 // INDEX - show all campgrounds
-app.get("/campgrounds", function(req, res) {
+app.get("/campgrounds", function (req, res) {
   // Get all campgrounds from DB
-  Campground.find({}, function(err, allCampgrounds) {
+  Campground.find({}, function (err, allCampgrounds) {
     if (err) {
       console.log(err);
     } else {
@@ -38,7 +62,7 @@ app.get("/campgrounds", function(req, res) {
 });
 
 // CREATE - add new campground to DB
-app.post("/campgrounds", function(req, res) {
+app.post("/campgrounds", function (req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
@@ -46,7 +70,7 @@ app.post("/campgrounds", function(req, res) {
 
   // Create a new campground and save to DB
   var newCampground = { name: name, image: image, description: desc };
-  Campground.create(newCampground, function(err, newlyCreated) {
+  Campground.create(newCampground, function (err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
@@ -57,16 +81,16 @@ app.post("/campgrounds", function(req, res) {
 });
 
 // NEW - show form to create new campground
-app.get("/campgrounds/new", function(req, res) {
+app.get("/campgrounds/new", function (req, res) {
   res.render("campgrounds/new");
 });
 
 // SHOW - show info about one campground
-app.get("/campgrounds/:id", function(req, res) {
+app.get("/campgrounds/:id", function (req, res) {
   // find the campground with provided ID
   Campground.findById(req.params.id)
     .populate("comments")
-    .exec(function(err, foundCampground) {
+    .exec(function (err, foundCampground) {
       if (err) {
         console.log(err);
       } else {
@@ -82,9 +106,9 @@ app.get("/campgrounds/:id", function(req, res) {
 // ===========================
 
 // NEW route
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", function (req, res) {
   // find campground by ID
-  Campground.findById(req.params.id, function(err, campground) {
+  Campground.findById(req.params.id, function (err, campground) {
     if (err) {
       console.log(err);
     } else {
@@ -94,9 +118,9 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
 });
 
 // CREATE route
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", function (req, res) {
   // lookup campground using id
-  Campground.findById(req.params.id, function(err, campground) {
+  Campground.findById(req.params.id, function (err, campground) {
     if (err) {
       console.log(err);
       res.redirect("/campgrounds");
@@ -104,7 +128,7 @@ app.post("/campgrounds/:id/comments", function(req, res) {
       // create new comment
       // connect new comment to campground
       // redirect to campground show page
-      Comment.create(req.body.comment, function(err, comment) {
+      Comment.create(req.body.comment, function (err, comment) {
         if (err) {
           console.log(err);
         } else {
@@ -118,7 +142,44 @@ app.post("/campgrounds/:id/comments", function(req, res) {
 });
 
 // ===========================
+// AUTH ROUTES
 // ===========================
-app.listen(3000, function() {
+
+// show register form
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function (req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
+// show login form
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {}
+);
+
+// ===========================
+// ===========================
+app.listen(3000, function () {
   console.log("YelpCamp Server has started...");
 });
